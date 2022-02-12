5617a98213f512c63f1b53dc064d7bf405b7a296
leonardops91 <leonardops91@gmail.com>
2022-02-12 19:26:52 -0300

    Retirado arquivo desnecessário

--- src/main/java/main.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class main {
-    public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("12345"));
-    }
-}
